f6b28a186c4326db6c041866aa3b312b91f383e2
Rakan Fasya Athhar Rayyan <rakan.fasya@ui.ac.id>
2024-03-08 14:41:16 +0700

    Feat: Merge request pudhil

--- backend/src/main/java/achievers/ieps/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/achievers/ieps/backend/security/WebSecurityConfig.java
@@ -39,8 +39,6 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/user/**").permitAll()
                         .requestMatchers("/api/login").permitAll()
-                        .requestMatchers("/api/berkas/**").permitAll()
-                        .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/user/detail-profil").hasAnyAuthority("PROCSTAFF", "PROCMANAGER")
                         .anyRequest().authenticated()
                 )
